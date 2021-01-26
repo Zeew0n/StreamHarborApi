79e587a2890e21c51f3264bed41cad6086ecc63e
Ashok Bhattarai <eashokbhattarai@gmail.com>
2021-01-26 09:50:10 +0545

    Added RoleName att. in Classes

--- WorkFlowTaskManager.Application/DTO/User/Response/UserListDTO.cs
+++ WorkFlowTaskManager.Application/DTO/User/Response/UserListDTO.cs
@@ -9,6 +9,7 @@ namespace WorkFlowTaskManager.Application.DTO.User.Response
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string Role { get; set; }
         public string Status { get; set; }
     }
